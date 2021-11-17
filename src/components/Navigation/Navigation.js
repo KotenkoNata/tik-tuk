d42c719bab071cae16a9247cf149c28a54915e25
KotenkoNata <35960689+KotenkoNata@users.noreply.github.com>
2021-11-17 20:27:27 +0200

    fix logo image src

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,7 +9,11 @@ const Navigation = () => {
       <header className={style["header"]}>
         <nav>
           <NavLink className={style["link-logo"]} to="/" exact>
-            <img className={style["logo"]} src="/images/logo.png" alt="Logo" />
+            <img
+              className={style["logo"]}
+              src={`${process.env.PUBLIC_URL}/images/logo.png`}
+              alt="Logo"
+            />
             <img
               src="https://fontmeme.com/permalink/211117/439580d4cdf5ccff3abfeae99d84338c.png"
               alt="TikTuk"
