bc65d51fca0d663a5c4ce2b67f45ec4f20ae96eb
KotenkoNata <kotenkonata8@gmail.com>
2021-12-21 15:06:19 +0200

    add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,17 @@ module.exports = {
     es6: true,
   },
   extends: ["react-app", "airbnb", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "import"],
+  settings: {
+    "import/extensions": [".js", ".jsx"],
+    "import/ignore": ["node_modules"],
+  },
   rules: {
+    "import/no-unresolved": [2, { commonjs: true, amd: true }],
+    "import/named": 2,
+    "import/namespace": 2,
+    "import/default": 2,
+    "import/export": 2,
     "arrow-body-style": ["off"],
     "no-restricted-exports": [
       "off",
