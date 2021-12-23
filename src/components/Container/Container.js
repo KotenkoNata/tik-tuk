06e2327a914ffd50a2151fd31688a8e1d268d34c
KotenkoNata <kotenkonata8@gmail.com>
2021-12-23 21:35:46 +0200

    fix: fix project according to PR comments (v4)

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 import styles from "./Container.module.css";
 
-const Container = ({ children }) => <div className={styles.Container}>{children}</div>;
+const Container = ({ children }) => <div className={styles.container}>{children}</div>;
 
 Container.propTypes = {
   children: PropTypes.node,
